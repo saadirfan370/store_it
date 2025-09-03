2ef1444a40104dd8366f583465f10eaaec17c9c2
Saad Irfan <saadirfan370@gmail.com>
2025-09-03 22:40:22 +0500

    add new icons, images, Tailwind theme config, and TS types

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-amber-100">
+      <h1 className="text-3xl text-brand">StoreIt - The only storage solution you need.</h1>
+    </div>
+  );
+}
